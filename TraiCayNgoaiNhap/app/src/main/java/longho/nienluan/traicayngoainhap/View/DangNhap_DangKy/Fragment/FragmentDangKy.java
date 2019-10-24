77c86138e43f1dcfd4a4f5bc61e25ffd45f55767
Long Ho <longho4697@gmail.com>
2019-10-24 23:41:34 +0700

    DangKy
    Nhập mã otp khi đăng ký

--- TraiCayNgoaiNhap/app/src/main/java/longho/nienluan/traicayngoainhap/View/DangNhap_DangKy/Fragment/FragmentDangKy.java
+++ TraiCayNgoaiNhap/app/src/main/java/longho/nienluan/traicayngoainhap/View/DangNhap_DangKy/Fragment/FragmentDangKy.java
@@ -15,8 +15,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -38,14 +40,18 @@ import longho.nienluan.traicayngoainhap.View.TrangChu.TrangChuActivity;
 
 public class FragmentDangKy extends Fragment implements ViewDangKy, View.OnClickListener, View.OnFocusChangeListener {
     PresenterLogicDangKy presenterLogicDangKy;
-    Button btnDangKy, btnDangKyFacebook;
-    EditText edtTenNguoiDung, edtEmail,edtSoDienThoai, edtDiaChi,edtMatKhau,edtNhapLaiMatKhau, edtCauHoi, edtCauTraLoi;
+    Button btnDangKy, btnDangKyFacebook,btnXacNhan;
+    EditText edtTenNguoiDung, edtEmail,edtSoDienThoai, edtDiaChi,edtMatKhau,edtNhapLaiMatKhau, txtOTP;
     RadioGroup rdgGioiTinh;
-    TextInputLayout layout_TenNguoiDung, layout_Email, layout_SoDienThoai, layout_DiaChi, layout_MatKhau, layout_NhapLaiMatKhau, layout_CauHoi, layout_CauTraLoi;
+    TextInputLayout layout_TenNguoiDung, layout_Email, layout_SoDienThoai, layout_DiaChi, layout_MatKhau, layout_NhapLaiMatKhau, layout_CauHoi, layout_CauTraLoi ;
     Boolean kiemtrathongtin = false;
     CallbackManager callbackManager;
     ModelDangNhap modelDangNhap;
-    String emaildk,matkhaudk;
+    String emaildk,matkhaudk,hoten,sodienthoai,diachi,nhaplaimatkhau;
+    LinearLayout lnOTP, lnDangKy;
+    nguoidung nguoiDung;
+    TextView txtThongBao,txtQuayLai;
+    int OTP;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -59,8 +65,8 @@ public class FragmentDangKy extends Fragment implements ViewDangKy, View.OnClick
         edtDiaChi = view.findViewById(R.id.edDiaChiDK);
         edtMatKhau = view.findViewById(R.id.edMatKhauDK);
         edtNhapLaiMatKhau = view.findViewById(R.id.edNhapLaiMatKhauDK);
-//        edtCauHoi = view.findViewById(R.id.edCauHoi);
-//        edtCauTraLoi = view.findViewById(R.id.edCauTraLoi);
+        txtOTP = view.findViewById(R.id.txtOTP);
+        btnXacNhan = view.findViewById(R.id.btnXacNhan);
         rdgGioiTinh = view.findViewById(R.id.rdg_GioiTinh);
         layout_TenNguoiDung = view.findViewById(R.id.input_edHoTenDK);
         layout_Email = view.findViewById(R.id.input_edEmailDK);
@@ -70,6 +76,10 @@ public class FragmentDangKy extends Fragment implements ViewDangKy, View.OnClick
         layout_NhapLaiMatKhau = view.findViewById(R.id.input_edNhapLaiMatKhauDK);
         layout_CauHoi = view.findViewById(R.id.input_edCauHoi);
         layout_CauTraLoi= view.findViewById(R.id.input_edCauTraLoi);
+        lnOTP = view.findViewById(R.id.lnOTP);
+        lnDangKy = view.findViewById(R.id.lnDangKy);
+        txtThongBao = view.findViewById(R.id.txtThongBao);
+        txtQuayLai = view.findViewById(R.id.txtQuayLai);
 
         presenterLogicDangKy = new PresenterLogicDangKy(this);
 
@@ -97,14 +107,14 @@ public class FragmentDangKy extends Fragment implements ViewDangKy, View.OnClick
 
         btnDangKy.setOnClickListener(this);
         btnDangKyFacebook.setOnClickListener(this);
+        btnXacNhan.setOnClickListener(this);
+        txtQuayLai.setOnClickListener(this);
         edtTenNguoiDung.setOnFocusChangeListener(this);
         edtEmail.setOnFocusChangeListener(this);
         edtSoDienThoai.setOnFocusChangeListener(this);
         edtDiaChi.setOnFocusChangeListener(this);
         edtMatKhau.setOnFocusChangeListener(this);
         edtNhapLaiMatKhau.setOnFocusChangeListener(this);
-//        edtCauHoi.setOnFocusChangeListener(this);
-//        edtCauTraLoi.setOnFocusChangeListener(this);
 
         return view;
     }
@@ -122,7 +132,7 @@ public class FragmentDangKy extends Fragment implements ViewDangKy, View.OnClick
 
     @Override
     public void DangKyThatBai() {
-        Toast.makeText(getActivity(), "Đăng ký thất bại", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "Tài khoản " + emaildk + " đã tồn tại", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -133,38 +143,38 @@ public class FragmentDangKy extends Fragment implements ViewDangKy, View.OnClick
             case R.id.btnDangKy:
                 btnDangKy();
                 break;
-        }
-        switch (id){
+            case R.id.btnXacNhan:
+                XacNhanOTP(String.valueOf(OTP));
+                break;
             case R.id.btnDangNhapFacebookDK:
                 LoginManager.getInstance().logInWithReadPermissions(FragmentDangKy.this, Arrays.asList("public_profile"));
                 break;
+            case R.id.txtQuayLai:
+                lnDangKy.setVisibility(View.VISIBLE);
+                lnOTP.setVisibility(View.GONE);
         }
 
     }
 
     private void btnDangKy(){
-        String hoten = edtTenNguoiDung.getText().toString();
+        hoten = edtTenNguoiDung.getText().toString();
         emaildk = edtEmail.getText().toString();
-        String sodienthoai = edtSoDienThoai.getText().toString();
-        String diachi = edtDiaChi.getText().toString();
+        sodienthoai = edtSoDienThoai.getText().toString();
+        diachi = edtDiaChi.getText().toString();
         matkhaudk = edtMatKhau.getText().toString();
-        String nhaplaimatkhau = edtNhapLaiMatKhau.getText().toString();
-//        String cauhoi = edtCauHoi.getText().toString();
-//        String cautraloi = edtCauTraLoi.getText().toString();
+        nhaplaimatkhau = edtNhapLaiMatKhau.getText().toString();
         int id = rdgGioiTinh.getCheckedRadioButtonId();
         RadioButton rad = getView().findViewById(id);
         String gioitinh = rad.getText().toString();
 
-        if(kiemtrathongtin) {
+        if(kiemtrathongtin()){
             int gt;
-            final nguoidung nguoiDung = new nguoidung();
+            nguoiDung = new nguoidung();
             nguoiDung.setTenNguoiDung(hoten);
             nguoiDung.setEmailND(emaildk);
             nguoiDung.setSoDienThoaiND(sodienthoai);
             nguoiDung.setDiaChiND(diachi);
             nguoiDung.setMatKhau(matkhaudk);
-//            nguoiDung.setCauHoi(cauhoi);
-//            nguoiDung.setCauTraLoi(cautraloi);
             if(gioitinh.equals("Nam"))
                 gt=1;
             else
@@ -172,161 +182,190 @@ public class FragmentDangKy extends Fragment implements ViewDangKy, View.OnClick
             nguoiDung.setGioiTinh(gt);
             nguoiDung.setMaQuyen(0);
 
-            presenterLogicDangKy.ThucHienDangKy(nguoiDung);
+            GenerateRandomNumber generateRandomNumber = new GenerateRandomNumber();
+            OTP = generateRandomNumber.RandomNumber();
 
+            SendMail sendMail = new SendMail(getActivity(),emaildk,"Xác nhận", "Mã xác nhận của bạn là: " + OTP);
+            sendMail.execute();
 
-//            GenerateRandomNumber generateRandomNumber = new GenerateRandomNumber();
-//            final int OTP = generateRandomNumber.RandomNumber();
-//
-//            SendMail sendMail = new SendMail(getActivity(),email,"Xác nhận", "Mã xác nhận của bạn là: " + OTP);
-//            sendMail.execute();
-//            //Builder thứ 2
-//            AlertDialog.Builder ad = new AlertDialog.Builder(getContext());
-//            ad.setTitle("Nhâp mã OTP");
-//            ad.setMessage("Vui lòng nhập mã vừa gửi đến email bạn!");
-//            final EditText edtOTP = new EditText(getContext());
-//            ad.setView(edtOTP);
-//            ad.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-//                public void onClick(DialogInterface dlg, int which) {
-//                    int otp = Integer.parseInt(edtOTP.getText().toString());
-//                    if(OTP == otp){
-//                        presenterLogicDangKy.ThucHienDangKy(nguoiDung);
-//                    }else{
-//                        Toast.makeText(getActivity(), "Mã OTP sai!", Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
-//            ad.show();
-        }else{
-            Log.d("kiemtra","Dang ky that bai ");
+            lnDangKy.setVisibility(View.GONE);
+            lnOTP.setVisibility(View.VISIBLE);
+            txtThongBao.setText("Đã gửi mã OTP đến email " + emaildk +"\nVui lòng nhập mã để hoàn thành đăng ký!");
+        } else{
+            Toast.makeText(getActivity(), "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
         }
-
     }
 
     @Override
     public void onFocusChange(View view, boolean b) {
 
         int id = view.getId();
-        switch (id){
-            case R.id.edHoTenDK:
-                if(!b){
-                    String chuoi = ((EditText)view).getText().toString();
-                    if(chuoi.trim().equals("") || chuoi.equals(null)){
-                        layout_TenNguoiDung.setError("Bạn chưa nhập mục này !");
-                        kiemtrathongtin = false;
-                    }else{
-                        layout_TenNguoiDung.setError(null);
-                        kiemtrathongtin = true;
-                    }
-                }
-                break;
-
-
-            case R.id.edEmailDK:
-                if(!b){
-
-                    String chuoi = ((EditText)view).getText().toString();
-
-                    if(chuoi.trim().equals("") || chuoi.equals(null)){
-                        layout_Email.setError("Bạn chưa nhập mục này !");
-                        kiemtrathongtin = false;
-                    }else{
-
-                        Boolean kiemtraemail = Patterns.EMAIL_ADDRESS.matcher(chuoi).matches();
-                        if(!kiemtraemail){
-                            layout_Email.setError("Đây không phải là địa chỉ Email !");
-                            kiemtrathongtin = false;
-                        }else{
-                            layout_Email.setError(null);
-                            kiemtrathongtin = true;
-                        }
-                    }
-                }
-                break;
-
-            case R.id.edDiaChiDK:
-                if(!b){
-                    String chuoi = ((EditText)view).getText().toString();
-                    if(chuoi.trim().equals("") || chuoi.equals(null)){
-                        layout_DiaChi.setError("Bạn chưa nhập mục này !");
-                        kiemtrathongtin = false;
-                    }else{
-                        layout_DiaChi.setError(null);
-                        kiemtrathongtin = true;
-                    }
-                }
-                break;
-
-            case R.id.edSoDienThoaiDK:
-                if(!b){
-                    String chuoi = ((EditText)view).getText().toString();
-                    if(chuoi.trim().equals("") || chuoi.equals(null)){
-                        layout_SoDienThoai.setError("Bạn chưa nhập mục này !");
-                        kiemtrathongtin = false;
-                    }else{
-                        layout_SoDienThoai.setError(null);
-                        kiemtrathongtin = true;
-                    }
-                }
-                break;
-
-            case R.id.edMatKhauDK:
-                if(!b){
-                    String chuoi = ((EditText)view).getText().toString();
-                    if(chuoi.trim().equals("") || chuoi.equals(null)){
-                        layout_MatKhau.setError("Bạn chưa nhập mục này !");
-                        kiemtrathongtin = false;
-                    }else{
-                        layout_MatKhau.setError(null);
-                        kiemtrathongtin = true;
-                    }
-                }
-                break;
-
-            case R.id.edNhapLaiMatKhauDK:
-                if(!b){
-                    String chuoi = ((EditText)view).getText().toString();
-                    String matkhau = edtMatKhau.getText().toString();
-                    if(!chuoi.equals(matkhau)){
-                        layout_NhapLaiMatKhau.setError("Mật khẩu không trùng khớp !");
-                        kiemtrathongtin = false;
-                    }else{
-                        layout_NhapLaiMatKhau.setError(null);
-                        kiemtrathongtin = true;
-                    }
-                }
-                ;break;
-
-//            case R.id.edCauHoi:
+//        switch (id){
+//            case R.id.edHoTenDK:
 //                if(!b){
 //                    String chuoi = ((EditText)view).getText().toString();
 //                    if(chuoi.trim().equals("") || chuoi.equals(null)){
-//                        layout_CauHoi.setError("Bạn chưa nhập mục này !");
+//                        layout_TenNguoiDung.setError("Bạn chưa nhập mục này !");
 //                        kiemtrathongtin = false;
 //                    }else{
-//                        layout_CauHoi.setError(null);
+//                        layout_TenNguoiDung.setError(null);
 //                        kiemtrathongtin = true;
 //                    }
 //                }
 //                break;
-//            case R.id.edCauTraLoi:
+//            case R.id.edEmailDK:
+//                if(!b){
+//
+//                    String chuoi = ((EditText)view).getText().toString();
+//
+//                    if(chuoi.trim().equals("") || chuoi.equals(null)){
+//                        layout_Email.setError("Bạn chưa nhập mục này !");
+//                        kiemtrathongtin = false;
+//                    }
+//                    Boolean kiemtraemail = Patterns.EMAIL_ADDRESS.matcher(chuoi).matches();
+//                    if(!kiemtraemail){
+//                        layout_Email.setError("Đây không phải là địa chỉ Email !");
+//                        kiemtrathongtin = false;
+//                    }else{
+//                        layout_Email.setError(null);
+//                        kiemtrathongtin = true;
+//                    }
+//
+//
+//                }
+//                break;
+//
+//            case R.id.edDiaChiDK:
 //                if(!b){
 //                    String chuoi = ((EditText)view).getText().toString();
 //                    if(chuoi.trim().equals("") || chuoi.equals(null)){
-//                        layout_CauTraLoi.setError("Bạn chưa nhập mục này !");
+//                        layout_DiaChi.setError("Bạn chưa nhập mục này !");
 //                        kiemtrathongtin = false;
 //                    }else{
-//                        layout_CauTraLoi.setError(null);
+//                        layout_DiaChi.setError(null);
 //                        kiemtrathongtin = true;
 //                    }
 //                }
 //                break;
-        }
+//
+//            case R.id.edSoDienThoaiDK:
+//                if(!b){
+//                    String chuoi = ((EditText)view).getText().toString();
+//                    if(chuoi.trim().equals("") || chuoi.equals(null)){
+//                        layout_SoDienThoai.setError("Bạn chưa nhập mục này !");
+//                        kiemtrathongtin = false;
+//                    }else{
+//                        layout_SoDienThoai.setError(null);
+//                        kiemtrathongtin = true;
+//                    }
+//                }
+//                break;
+//
+//            case R.id.edMatKhauDK:
+//                if(!b){
+//                    String chuoi = ((EditText)view).getText().toString();
+//                    if(chuoi.trim().equals("") || chuoi.equals(null)){
+//                        layout_MatKhau.setError("Bạn chưa nhập mục này !");
+//                        kiemtrathongtin = false;
+//                    }else{
+//                        layout_MatKhau.setError(null);
+//                        kiemtrathongtin = true;
+//                    }
+//                }
+//                break;
+//
+//            case R.id.edNhapLaiMatKhauDK:
+//                if(!b){
+//                    String chuoi = ((EditText)view).getText().toString();
+//                    String matkhau = edtMatKhau.getText().toString();
+//                    if(!chuoi.equals(matkhau)){
+//                        layout_NhapLaiMatKhau.setError("Mật khẩu không trùng khớp !");
+//                        kiemtrathongtin = false;
+//                    }else{
+//                        layout_NhapLaiMatKhau.setError(null);
+//                        kiemtrathongtin = true;
+//                    }
+//                }
+//                break;
+//        }
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
 
+    }
+
+    public void XacNhanOTP(String OTP){
+        String strOTP = String.valueOf(txtOTP.getText());
+        if(strOTP.length()>0){
+            if(OTP.equals(strOTP)){
+                presenterLogicDangKy.ThucHienDangKy(nguoiDung);
+            }
+            else {
+                Toast.makeText(getActivity(), "Mã OTP chưa đúng", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(getActivity(), "Bạn chưa nhập mã OTP", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public boolean kiemtrathongtin() {
+        boolean kiemtra = true;
+        if (hoten.trim().length() == 0) {
+            layout_TenNguoiDung.setError("Chưa nhập họ tên");
+            edtTenNguoiDung.requestFocus();
+            kiemtra &= false;
+        } else {
+            layout_TenNguoiDung.setError(null);
+        }
+        Boolean kiemtraemail = Patterns.EMAIL_ADDRESS.matcher(emaildk).matches();
+        if (emaildk.trim().length() == 0) {
+            layout_Email.setError("Chưa nhập email");
+            edtTenNguoiDung.requestFocus();
+            kiemtra &= false;
+        } else if(!kiemtraemail) {
+            layout_Email.setError("Đây không phải là địa chỉ Email !");
+            kiemtra &= false;
+        }else {
+            layout_Email.setError(null);
+        }
+        if (diachi.trim().length() == 0) {
+            layout_DiaChi.setError("Chưa nhập địa chỉ");
+            edtDiaChi.requestFocus();
+            kiemtra &= false;
+        } else {
+            layout_DiaChi.setError(null);
+        }
+        if (sodienthoai.trim().length() == 0) {
+            layout_SoDienThoai.setError("Chưa nhập số điện thoại");
+            edtSoDienThoai.requestFocus();
+            kiemtra &= false;
+        } else if(sodienthoai.trim().length() <8 || sodienthoai.trim().length() >11) {
+            layout_SoDienThoai.setError("Độ dài hợp lệ từ 8 - 11 chữ số");
+            edtSoDienThoai.requestFocus();
+            kiemtra &= false;
+        }else{
+            layout_SoDienThoai.setError(null);
+        }
+
+        if (matkhaudk.trim().length() == 0) {
+            layout_MatKhau.setError("Chưa nhập mật khẩu");
+            edtMatKhau.requestFocus();
+            kiemtra &= false;
+        } else {
+            layout_MatKhau.setError(null);
+        }
+        if (nhaplaimatkhau.trim().length() == 0) {
+            layout_NhapLaiMatKhau.setError("Bạn phải nhập lại mật khẩu");
+            edtNhapLaiMatKhau.requestFocus();
+            kiemtra &= false;
+        } if(!matkhaudk.equals(nhaplaimatkhau)){
+            layout_NhapLaiMatKhau.setError("Mật khẩu chưa trùng khớp");
+            kiemtra &= false;
+        } else {
+            layout_NhapLaiMatKhau.setError(null);
+        }
+        return kiemtra;
     }
 }
